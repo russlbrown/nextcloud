cfe9bafa14b8f2e5042a9ee1834eaff2ddcda5ac
Russ Brown <russbrown@protonmail.com>
2019-03-16 22:44:28 -0600

    backup.sh works (I think). restore.sh has issues

---
+++ restore.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+
+#
+# Bash script for restoring backups of Nextcloud.
+# Usage: 
+#   - With backup directory specified in the script: ./NextcloudRestore.sh <BackupName> (e.g. ./NextcloudRestore.sh 20170910_132703)
+#   - With backup directory specified by parameter: ./NextcloudRestore.sh <BackupName> <BackupDirectory> (e.g. ./NextcloudRestore.sh 20170910_132703 /media/hdd/nextcloud_backup)
+#
+# The script is based on an installation of Nextcloud using nginx and MariaDB, see https://decatec.de/home-server/nextcloud-auf-ubuntu-server-18-04-lts-mit-nginx-mariadb-php-lets-encrypt-redis-und-fail2ban/
+#
+
+#
+# IMPORTANT
+# You have to customize this script (directories, users, etc.) for your actual environment.
+# All entries which need to be customized are tagged with "TODO".
+#
+
+# Variables
+restore=$1
+backupMainDir=$2
+
+echo "Importing .env"
+source .env
+echo "POSTGRES_DB: $POSTGRES_DB"
+
+if [ -z "$backupMainDir" ]; then
+	# TODO: The directory where you store the Nextcloud backups (when not specified by args)
+    backupMainDir="/media/russ/silver_hdd/nextcloud_backups"
+fi
+
+echo "Backup directory: $backupMainDir"
+
+currentRestoreDir="${backupMainDir}/${restore}"
+
+# TODO: The directory of your Nextcloud installation (this is a directory under your web root)
+nextcloudFileDir="/var/lib/docker/volumes/nextcloud_nextcloud"
+
+# TODO: The directory of your Nextcloud data directory (outside the Nextcloud file directory)
+# If your data directory is located under Nextcloud's file directory (somewhere in the web root), the data directory should not be restored separately
+# nextcloudDataDir="/var/nextcloud_data"
+
+# TODO: The directory of your Nextcloud's local external storage.
+# Uncomment if you use local external storage.
+#nextcloudLocalExternalDataDir="/var/nextcloud_external_data"
+
+# TODO: The service name of the web server. Used to start/stop web server (e.g. 'systemctl start <webserverServiceName>')
+webserverServiceName="nginx"
+
+# TODO: Your Nextcloud database name
+nextcloudDatabase="$POSTGRES_DB"
+
+# TODO: Your Nextcloud database user
+dbUser="$POSTGRES_USER"
+
+# TODO: The password of the Nextcloud database user
+dbPassword="$POSTGRES_PASSWORD"
+
+# TODO: Your web server user
+webserverUser="www-data"
+
+# File names for backup files
+# If you prefer other file names, you'll also have to change the NextcloudBackup.sh script.
+fileNameBackupFileDir="nextcloud-filedir.tar.gz"
+#fileNameBackupDataDir="nextcloud-datadir.tar.gz"
+
+# TOOD: Uncomment if you use local external storage
+#fileNameBackupExternalDataDir="nextcloud-external-datadir.tar.gz"
+
+fileNameBackupDb="nextcloud-db.sql"
+
+# Function for error messages
+errorecho() { cat <<< "$@" 1>&2; }
+
+#
+# Check if parameter(s) given
+#
+if [ $# != "1" ] && [ $# != "2" ]
+then
+    errorecho "ERROR: No backup name to restore given, or wrong number of parameters!"
+    errorecho "Usage: NextcloudRestore.sh 'BackupDate' ['BackupDirectory']"
+    exit 1
+fi
+
+#
+# Check for root
+#
+if [ "$(id -u)" != "0" ]
+then
+    errorecho "ERROR: This script has to be run as root!"
+    exit 1
+fi
+
+#
+# Check if backup dir exists
+#
+if [ ! -d "${currentRestoreDir}" ]
+then
+	 errorecho "ERROR: Backup ${restore} not found!"
+    exit 1
+fi
+
+#
+# Set maintenance mode
+#
+echo "Set maintenance mode for Nextcloud..."
+sudo docker-compose exec --user "${webserverUser}" app php occ maintenance:mode --on
+echo "Done"
+echo
+
+#
+# Stop web server
+#
+echo "Stopping proxy and letsencrypt-companion docker containers..."
+# systemctl stop "${webserverServiceName}"
+docker-compose stop proxy letsencrypt-companion
+echo "Done"
+echo
+
+#
+# Delete old Nextcloud direcories
+#
+
+# File directory
+echo "Deleting old Nextcloud file directory..."
+rm -r "${nextcloudFileDir}"
+mkdir -p "${nextcloudFileDir}"
+echo "Done"
+echo
+
+# # Data directory
+# echo "Deleting old Nextcloud data directory..."
+# rm -r "${nextcloudDataDir}"
+# mkdir -p "${nextcloudDataDir}"
+# echo "Done"
+# echo
+
+# Local external storage
+# TOOD: Uncomment if you use local external storage
+#echo "Deleting old Nextcloud local external storage directory..."
+#rm -r "${nextcloudLocalExternalDataDir}"
+#mkdir -p "${nextcloudLocalExternalDataDir}"
+#echo "Done"
+#echo
+
+#
+# Restore file and data directory
+#
+
+# File directory
+echo "Restoring Nextcloud file directory..."
+tar -xmpzf "${currentRestoreDir}/${fileNameBackupFileDir}" -C "${nextcloudFileDir}"
+echo "Done"
+echo
+
+# # Data directory
+# echo "Restoring Nextcloud data directory..."
+# tar -xmpzf "${currentRestoreDir}/${fileNameBackupDataDir}" -C "${nextcloudDataDir}"
+# echo "Done"
+# echo
+
+#
+# Restore database
+#
+echo "Dropping old Nextcloud DB..."
+# MySQL/MariaDB:
+##mysql -h localhost -u "${dbUser}" -p"${dbPassword}" -e "DROP DATABASE ${nextcloudDatabase}"
+sudo docker-compose exec db dropdb -U "${dbUser}" "${nextcloudDatabase}"
+# PostgreSQL (uncomment if you are using PostgreSQL as Nextcloud database)
+
+echo "Done"
+echo
+
+
+echo "Creating new DB for Nextcloud..."
+# MySQL/MariaDB:
+# Use this if the databse from the backup uses UTF8 with multibyte support (e.g. for emoijs in filenames):
+# mysql -h localhost -u "${dbUser}" -p"${dbPassword}" -e "CREATE DATABASE ${nextcloudDatabase} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
+# Use this if the database from the backup DOES NOT use UTF8 with multibyte support (e.g. for emoijs in filenames):
+#mysql -h localhost -u "${dbUser}" -p"${dbPassword}" -e "CREATE DATABASE ${nextcloudDatabase}"
+
+# PostgreSQL (uncomment if you are using PostgreSQL as Nextcloud database)
+# sudo -u postgres psql -c "CREATE DATABASE ${nextcloudDatabase} WITH OWNER ${dbUser} TEMPLATE template0 ENCODING \"UTF8\";"
+sudo docker-compose exec db createdb --username="${dbUser}" --owner="${dbUser}" --template=template0 --encoding=UTF8 "${nextcloudDatabase}"
+echo "Done"
+echo
+
+echo "Restoring backup DB..."
+# MySQL/MariaDB:
+# mysql -h localhost -u "${dbUser}" -p"${dbPassword}" "${nextcloudDatabase}" < "${currentRestoreDir}/${fileNameBackupDb}"
+
+# PostgreSQL (uncomment if you are using PostgreSQL as Nextcloud database)
+# sudo -u postgres psql "${nextcloudDatabase}" < "${currentRestoreDir}/${fileNameBackupDb}"
+sudo docker-compose exec --user "${dbUser}" db psql "${nextcloudDatabase}" < "${currentRestoreDir}/${fileNameBackupDb}"
+echo "Done"
+echo
+
+#
+# Start web server ()
+#
+echo "Starting proxy and letsencrypt containers..."
+# systemctl start "${webserverServiceName}"
+docker-compose up -d proxy letsencrypt-companion
+echo "Done"
+echo
+
+#
+# Set directory permissions
+#
+echo "Setting directory permissions..."
+chown -R "${webserverUser}":"${webserverUser}" "${nextcloudFileDir}"
+# chown -R "${webserverUser}":"${webserverUser}" "${nextcloudDataDir}"
+# TOOD: Uncomment if you use local external storage
+#chown -R "${webserverUser}":"${webserverUser}" "${nextcloudLocalExternalDataDir}"
+echo "Done"
+echo
+
+#
+# Update the system data-fingerprint (see https://docs.nextcloud.com/server/15/admin_manual/configuration_server/occ_command.html#maintenance-commands-label)
+#
+echo "Updating the system data-fingerprint..."
+# sudo -u "${webserverUser}" php ${nextcloudFileDir}/occ maintenance:data-fingerprint
+sudo docker-compose exec --user "${webserverUser}" app php ${nextcloudFileDir}/occ maintenance:data-fingerprint
+echo "Done"
+echo
+
+
+
+
+#
+# Disbale maintenance mode
+#
+
+function DisableMaintenanceMode() {
+	echo "Switching off maintenance mode..."
+	sudo docker-compose exec --user "${webserverUser}" app php occ maintenance:mode --off
+	echo "Done"
+	echo
+}
+DisableMaintenanceMode
+
+echo
+echo "DONE!"
+echo "Backup ${restore} successfully restored."
